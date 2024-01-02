5cb5c436234c2d758d1026322f4606584d54d0ab
JOHN CARTER <mukulsingh2276@gmail.com>
2024-01-02 13:55:29 +0530

    2

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
     cors : {
-        origin: "http://localhost:3000"
+        origin: "https://chat-app-client-ae5l.vercel.app/"
     }
 });
 
